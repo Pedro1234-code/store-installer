3bc66c9db1d416b33b709a1a70e544bb42b1ab7e
winux <78425126+Pedro1234-code@users.noreply.github.com>
2022-01-08 13:40:10 -0300

    Add files via upload

---
+++ Execute.ps1
@@ -0,0 +1,13 @@
+echo StoreInstaller V1
+
+echo Windows 64-bits
+
+add-appxpackage Microsoft.NET.Native.Framework.2.2_2.2.29512.0_x64__8wekyb3d8bbwe.Appx
+
+add-appxpackage Microsoft.NET.Native.Runtime.2.2_2.2.28604.0_x64__8wekyb3d8bbwe.Appx
+
+add-appxpackage Microsoft.UI.Xaml.2.7_7.2109.13004.0_x64__8wekyb3d8bbwe.Appx
+
+add-appxpackage Microsoft.WindowsStore_22112.1401.2.0_neutral___8wekyb3d8bbwe.Msixbundle
+
+Echo MS Store installed
